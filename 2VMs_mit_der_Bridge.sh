de803c8ba70bbf5c53e63498c71d5f59e860c361
monika-grzeskiewicz <64071184+monika-grzeskiewicz@users.noreply.github.com>
2020-04-22 15:33:43 +0200

    Create 2VMs_mit_der_Bridge.sh

---
+++ 2VMs_mit_der_Bridge.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#create 1. VM and interface
+
+bash basis-script.sh 1 VM1
+sleep 30
+bash basis-script.sh 4 VM1 08:00:00:00:00:01
+
+#create 2. VM and interface
+
+bash basis-script.sh 1 VM2
+sleep 30
+bash basis-script.sh 4 VM2 08:00:00:00:00:02
+
+#create bridge
+
+bash basis-script.sh 3 br00-8 10.10.80.1 255.255.255.0 10.10.80.10 10.10.80.80 08:00:00:00:00:01 10.10.80.11 08$
+
+#add interfaces to the bridge
+
+bash basis-script.sh 5 VM1 br00-8
+bash basis-script.sh 5 VM2 br00-8
