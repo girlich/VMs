c01ac9a5b3e42f312b386f862785d0bf0d164ced
monika-grzeskiewicz <64071184+monika-grzeskiewicz@users.noreply.github.com>
2020-04-23 19:18:24 +0200

    Update 2VMs_mit_der_Bridge.sh

--- 2VMs_mit_der_Bridge.sh
+++ 2VMs_mit_der_Bridge.sh
@@ -8,17 +8,21 @@ VM2_IP=10.10.80.12
 #create 1. VM and interface
 ./basis-script.sh 1 VM1
 sleep 30
-./basis-script.sh 4 VM1 ${VM1_MAC}
+./basis-script.sh 4 VM1
 
 #create 2. VM and interface
 ./basis-script.sh 1 VM2
 sleep 30
-./basis-script.sh 4 VM2 ${VM2_MAC}
+./basis-script.sh 4 VM2 
 
 #create bridge
-./basis-script.sh 3 br00-8 10.10.80.1 255.255.255.0 10.10.80.10 10.10.80.80 ${VM1_MAC} ${VM1_IP} ${VM2_MAC} ${VM2_IP}
+./basis-script.sh 3 br-1 10.10.80.1 255.255.255.0 10.10.80.10 10.10.80.80
+
+#add a dhcp static host entry to the network
+./basis-script.sh 7 br-1 ${VM1_MAC} ${VM1_IP}
+./basis-script.sh 7 br-1 ${VM2_MAC} ${VM2_IP}
 
 #add interfaces to the bridge
+./basis-script.sh 5 VM1 br-1 ${VM1_MAC}
+./basis-script.sh 5 VM2 br-1 ${VM2_MAC}
 
-./basis-script.sh 5 VM1 br00-8
-./basis-script.sh 5 VM2 br00-8
