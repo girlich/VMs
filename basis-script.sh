8e56a3248d2ff06c209258dbb126910966dee254
monika-grzeskiewicz <64071184+monika-grzeskiewicz@users.noreply.github.com>
2020-04-23 19:23:41 +0200

    Update basis-script.sh

--- basis-script.sh
+++ basis-script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 case $1 in
   1)    #create VM
-        uvt-kvm create $2;;
+        uvt-kvm create $2 release=bionic
+        ;;
 
   2)    #destroy VM
-        uvt-kvm destroy  $2;;
+        uvt-kvm destroy  $2 
+        ;;
 
   3)    #create bridge
         printf  "<network>
@@ -13,35 +15,42 @@ case $1 in
                   <ip address='$3' netmask='$4'>
                     <dhcp>
                       <range start='$5' end='$6'/>
-                        <host mac='$7' ip='$8'/>
-                        <host mac='$9'  ip='${10}'/>
-                    </dhcp>
+            </dhcp>
                   </ip>
                 </network>"              >> $2.xml
 
         virsh net-define $2.xml
         virsh net-start $2
-        virsh net-autostart $2;;
+        virsh net-autostart $2
+        ;;
+
 
   4)    #VM interface
         uvt-kvm ssh $2 'sudo apt-get install ifupdown'
-        uvt-kvm ssh $2 "sudo chmod 777 /etc/network/interfaces"
+        uvt-kvm ssh $2 "sudo chmod  777 /etc/network/interfaces"
         uvt-kvm ssh $2 "sudo echo auto ens7 >> /etc/network/interfaces"
         uvt-kvm ssh $2 "sudo echo iface ens7 inet dhcp >> /etc/network/interfaces"
         uvt-kvm ssh $2 "sudo echo        >> /etc/network/interfaces"
         uvt-kvm ssh $2 "sudo echo auto lo >> /etc/network/interfaces"
         uvt-kvm ssh $2 "sudo echo iface lo inet loopback >> /etc/network/interfaces"
-        uvt-kvm ssh $2 "sudo echo        >> /etc/network/interfaces"
-        uvt-kvm ssh $2 "sudo echo iface ens7 inet dhcp >> /etc/network/interfaces"
-        uvt-kvm ssh $2 "sudo echo        hwaddress ether $3 >> /etc/network/interfaces"
         ;;
 
   5)    #attach interface to the bridge
-        sudo virsh attach-interface --domain $2 --type bridge --source $3 --model virtio --config --live
-
+        sudo virsh attach-interface --domain $2 --type network --source $3 --model virtio --mac $4 --config --live
         ;;
 
-  6)    #delete bridge
+  6)    #detach -interface
+        sudo virsh attach-interface --domain $2 --type network --mac $3 --config
+        ;;
+
+  7)    #add a dhcp static host entry to the network
+        virsh net-update $2 add ip-dhcp-host \
+          "<host mac='$3' \
+           ip='$4' />" \
+           --live --config
+        ;;
+
+  8)    #delete bridge
         sudo ifconfig $2 down
         sudo brctl delbr $2
         sudo virsh net-destroy $2
